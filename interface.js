92e9bbad87c6c9c918ff59b7941fbb64fae01469
Mohamed Amine <mohamedaminebenhmida4@gmail.com>
2023-09-08 18:59:15 +0100

    decorators

--- interface.js
+++ interface.js
@@ -7,3 +7,8 @@ let [user1, user2, ...restusers] = [{ name: "paoulo", id: 1, email: "" }, { name
 /*console.log(user2);*/
 let usersup = restusers.filter((user) => user.id > 3);
 console.log(usersup);
+class component {
+    constructor(name) {
+        this.name = name;
+    }
+}
--- interface.ts
+++ interface.ts
@@ -15,3 +15,8 @@ let [user1,user2,...restusers]:User[]=[{name:"paoulo",id:1,email:""},{name:"paou
 /*console.log(user2);*/
 let usersup=restusers.filter((user)=>user.id>3);
 console.log(usersup);
+class component { 
+    constructor(public name:string){
+        
+    }
+}
